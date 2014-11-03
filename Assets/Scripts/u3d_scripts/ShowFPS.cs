01ef254f48c8af6973fe1bd1a09928e84176c64c
kbengine <liquidx@163.com>
2014-11-03 11:00:54 +0800

    up

---
+++ Assets/Scripts/u3d_scripts/ShowFPS.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowFPS : MonoBehaviour {
+
+    public float f_UpdateInterval = 0.5F;
+
+    private float f_LastInterval;
+
+    private int i_Frames = 0;
+
+    private float f_Fps;
+
+    void Start()
+    {
+		//Application.targetFrameRate=60;
+
+        f_LastInterval = Time.realtimeSinceStartup;
+
+        i_Frames = 0;
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(Screen.width - 40, 1, 200, 200), f_Fps.ToString("f2"));
+    }
+
+    void Update()
+    {
+        ++i_Frames;
+
+        if (Time.realtimeSinceStartup > f_LastInterval + f_UpdateInterval)
+        {
+            f_Fps = i_Frames / (Time.realtimeSinceStartup - f_LastInterval);
+
+            i_Frames = 0;
+
+            f_LastInterval = Time.realtimeSinceStartup;
+        }
+    }
+}
