8b091586964d11315dd3fe16cc85b5b8b1ab5379
kebiao <380000937@qq.com>
2018-07-12 18:41:53 +0800

    up

--- Assets/Scripts/kbe_scripts/skills/Skill.cs
+++ Assets/Scripts/kbe_scripts/skills/Skill.cs
@@ -12,14 +12,21 @@ namespace KBEngine
     	public Int32 id;
     	public float canUseDistMin = 0f;
     	public float canUseDistMax = 3f;
-    	
-		public Skill()
+
+        // 最后一次使用时间， 最好的做法是给客户端实体增加cooldown系统，由于demo只是简单展示，就这样简单实现功能
+    	public System.DateTime lastUsedTime = System.DateTime.Now;
+
+        public Skill()
 		{
 		}
 		
 		public bool validCast(KBEngine.Entity caster, SCObject target)
 		{
-			float dist = Vector3.Distance(target.getPosition(), caster.position);
+            TimeSpan span = DateTime.Now - lastUsedTime;
+            if (span.TotalMilliseconds < 300)
+                return false;
+
+            float dist = Vector3.Distance(target.getPosition(), caster.position);
 			if(dist > canUseDistMax)
 				return false;
 			
@@ -29,6 +36,7 @@ namespace KBEngine
 		public void use(KBEngine.Entity caster, SCObject target)
 		{
 			caster.cellCall("useTargetSkill", id, ((SCEntityObject)target).targetID);
-		}
+            lastUsedTime = System.DateTime.Now;
+        }
     }
 } 
