3745d794a946a7cd2f6daf93b366cf796fdec8ce
kebiao <380000937@qq.com>
2019-02-15 10:33:18 +0800

    https://github.com/kbengine/kbengine/issues/1045

---
+++ Assets/Standard Assets/Character Controllers/Sources/Scripts/FPSInputController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterMotor))]
+[AddComponentMenu("Character/FPS Input Controller")]
+
+public class FPSInputController : MonoBehaviour
+{
+    private CharacterMotor motor;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        motor = gameObject.GetComponent<CharacterMotor>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var directionVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        if (directionVector != Vector3.zero)
+        {
+            // Get the length of the directon vector and then normalize it
+            // Dividing by the length is cheaper than normalizing when we already have the length anyway
+            var directionLength = directionVector.magnitude;
+            directionVector = directionVector / directionLength;
+
+            // Make sure the length is no bigger than 1
+            directionLength = Mathf.Min(1, directionLength);
+
+            // Make the input vector more sensitive towards the extremes and less sensitive in the middle
+            // This makes it easier to control slow speeds when using analog sticks
+            directionLength = directionLength * directionLength;
+
+            // Multiply the normalized direction vector by the modified length
+            directionVector = directionVector * directionLength;
+        }
+
+        // Apply the direction to the CharacterMotor
+        motor.inputMoveDirection = transform.rotation * directionVector;
+        motor.inputJump = Input.GetButton("Jump");
+    }
+}
