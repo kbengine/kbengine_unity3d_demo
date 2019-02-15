013d6eb1a97ee97042eea21c1da0d539ef7b782d
chenlinyan <874499648@qq.com>
2019-02-15 10:02:21 +0800

    #替换资源文件中的移动模块的.js文件

---
+++ Assets/tsetDemo.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tsetDemo : MonoBehaviour
+{
+    public float speed = 6.0F;
+
+    public float jumpSpeed = 8.0F;
+
+    public float gravity = 20.0F;
+
+    private Vector3 moveDirection = Vector3.zero;
+    CharacterController moveObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveObj = gameObject.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CharacterController controller = GetComponent<CharacterController>();
+        if (controller.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+
+            if (Input.GetButton("Jump"))
+                moveDirection.y = jumpSpeed;
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
+
+
+
+
